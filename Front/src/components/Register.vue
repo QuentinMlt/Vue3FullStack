3e914f72ebbb6f25affd6b491593875482e4a4ae
QuentinMlt <quentinmaillotpro@gmail.com>
2022-06-20 15:44:07 +0200

    ajout systeme d'inscription

--- Front/src/components/Register.vue
+++ Front/src/components/Register.vue
@@ -1,6 +1,23 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+import {useUserStore} from "../services/userStore.js";
+
+const {user} = useUserStore();
+
+const {register} = useUserStore();
+
+register("test","test","test")
+
+</script>
 
 <template>
-    <h1>Ne fais rien</h1>
+    <h1>formulaire d'inscription</h1>
+
+    
+        <input type="email" v-model="email" placeholder="email"/>
+        <input type="text" v-model="username"  placeholder="username"/>
+        <input type="password" v-model="password"  placeholder="password"/>
+        <button @click="register(email, username, password)">S'inscrire</button>
+
 </template>
 
